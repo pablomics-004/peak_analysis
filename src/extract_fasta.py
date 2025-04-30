442d68496f5927b12cb1062fcd660376100698c0
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-04-29 22:09:51 -0600

    Función: leyendo el archivo de picos. Primer planteamiento.

--- src/extract_fasta.py
+++ src/extract_fasta.py
@@ -52,5 +52,49 @@ def cargar_genoma(fasta_ruta):
 
 def leer_archivo_picos(peaks_ruta):
     """
+    Args:
+        peaks_ruta: ruta al archivo de picos.
+    Return:
+        Diccionario TF-lista de (peak_start,peak_end)
     """
-    ...
+    
+    # Verificando la presencia del archivo
+    if not os.path.isfile(peaks_ruta):
+        raise FileNotFoundError(f'Error: no se encontró el archivo: {peaks_ruta}')
+    
+    # Campos que debe tener
+    campos = ['TF_name','Peak_start','Peak_end']
+    
+    try:
+        
+        with open(peaks_ruta) as archivo:
+
+            # Verificando que no falte alguno de los parámetros
+            encabezado = archivo.readline().strip('\n').split('\t')
+            if not all(campo in encabezado for campo in campos):
+                raise ValueError(f'El archivo no cuenta con alguno de los campos TF_name, Peak_start o Peak_end.')
+
+            # Diccionario e índices
+            dicc_picos = {}
+            ind_tf = encabezado.index(campos[0])
+            ind_ps = encabezado.index(campos[1])
+            ind_pe = encabezado.index(campos[2])
+
+            next(archivo)
+            for linea in archivo:
+                actual = linea.strip().split('\t')
+                
+                # Creando e inicializando la llave
+                if actual[ind_tf] not in dicc_picos:
+                    dicc_picos[actual[ind_tf]] = list()
+                
+                # Verificando el orden de Peak_start y Peak_end
+                if (int(actual[ind_pe]) - int(actual[ind_ps])) < 0:
+                    raise ValueError(f'Los campos Peak_end y Peak_start son incorrectos en el TF {actual[ind_tf]}')
+                
+                dicc_picos[actual[ind_tf]].append((actual[ind_ps],actual[ind_pe]))
+            
+            return dicc_picos
+
+    except OSError as e:
+        raise RuntimeError(f'Error al leer el archivo de picos: {peaks_ruta}') from e
